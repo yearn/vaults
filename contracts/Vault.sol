12f81c33dfb34286d778c3a6ede8abab3a785844
Bryant <fubuloubu@gmail.com>
2020-09-10 16:09:07 -0400

    refactor: Vault is a Token

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.2;
 
-interface Vault {
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface Vault is IERC20 {
     function token() external view returns (address);
     function claimInsurance() external; // NOTE: Only yDelegatedVault implements this
     function getPricePerFullShare() external view returns (uint);
